87ff19e7eedc5c26db47fb6b2da41209f8877a83
vzhz <vehanus@gmail.com>
2017-04-08 21:04:21 -0400

    add function to save data to txt file, learned about cursors, primatives, and reading and writing files

--- timer.py
+++ timer.py
@@ -1,5 +1,7 @@
 import readchar
 import time
+import yaml
+import os
 
 ## global vars like being on top ##
 is_counting = False
@@ -24,9 +26,20 @@ def toggle_counting():
     else:
         counting_false()
 
+def save_data(elapsed_time, form_time_sec_filepath):
+    if os.path.exists(form_time_sec_filepath):
+        append_write = "a+" # append if already exists
+    else:
+        append_write = "w+" # make a new file if it doesn't exist
+    with open(form_time_sec_filepath, append_write) as form_time_sec_file:
+        form_time_sec_file.write(str(elapsed_time) + "\n")
+        form_time_sec_file.seek(0) # move file cursor from the end of the file to the beginning so we can read it
+        print form_time_sec_file.read()
+
 while (True):
     saved_char = readchar.readchar()
     if saved_char == "\x03" or saved_char == "\x04" or saved_char == "\x05":
+        save_data()
         break
     if saved_char != " ":
         print "You didn't press space, you pressed: " + saved_char
